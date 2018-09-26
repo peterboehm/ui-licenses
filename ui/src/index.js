d4249349ae0a57d699f208a1529eee821541fc09
Ian Ibbotson <ian.ibbotson@k-int.com>
2018-09-26 10:11:03 +0100

    Clear down ready for rework

--- ui/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { hot } from 'react-hot-loader'
-import Routing from './routing'
-import 'whatwg-fetch'
-import '../scss/styles.scss'
-
-export default hot(module)(Routing)
