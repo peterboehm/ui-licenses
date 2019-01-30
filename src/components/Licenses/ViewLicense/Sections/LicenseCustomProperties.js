4683acd70cda9f54af42cc6adbadbf84bdfaf741
Mark Deutsch <mark.deutsch@duke.edu>
2019-01-30 13:15:19 -0500

    Custom Properties: Cleanup and regex fixes

--- src/components/Licenses/ViewLicense/Sections/LicenseCustomProperties.js
+++ src/components/Licenses/ViewLicense/Sections/LicenseCustomProperties.js
@@ -1,69 +1,68 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
-import {
-  Accordion,
-  Col,
-  Row,
-} from '@folio/stripes/components';
-import css from './LicenseCustomProperties.css';
+import { Accordion, Col, Layout, Row } from '@folio/stripes/components';
 
+export default class LicenseCustomProperties extends React.Component {
+  static propTypes = {
+    license: PropTypes.shape({ customProperties: PropTypes.object }),
+    id: PropTypes.string,
+    onToggle: PropTypes.func,
+    open: PropTypes.bool,
+  };
 
-const camelCaseSplit = /([A-Z])(?![A-Z])/gm;
+  renderPropertyName = (propName) => {
+    const afterLowercase = /(?<=[a-z])([A-Z])/gm;
+    const afterCapsBeforeLowercase = /(?<=[A-Z])([A-Z])(?=[a-z])/gm
 
-const nicePropertyName = (pName) => {
-  const str = pName.replace(camelCaseSplit, ' $1');
-  return str.charAt(0).toUpperCase() + str.slice(1);
-};
+    let formattedName = propName
+      .replace(afterLowercase, ' $1')
+      .replace(afterCapsBeforeLowercase, ' $1')
+      .replace('_', ' ');
 
-const renderCustomPropertyValue = (type, value) => {
-  if (type.endsWith('CustomPropertyRefdata')) {
-    // output value.value
-    return value.value;
-  }
+    formattedName = formattedName.charAt(0).toUpperCase() + formattedName.slice(1);
 
-  return value;
-};
-
-const renderCustomPropertyValues = (entries) => {
-  const renderedValues = [];
-  entries.forEach((entry, index) => {
-    renderedValues.push(
-      <Col xs={12} md={8} mdOffset={((index === 0) ? null : 4)} className={(index > 0 ? 'subsequentRow' : null)}>
-        {renderCustomPropertyValue(entry.type, entry.value)}
+    return (
+      <Col xs={12} md={4}>
+        <strong>{formattedName}</strong>
       </Col>
     );
-  });
+  }
 
-  return renderedValues;
-};
+  renderPropertyValues = (propValues) => {
+    return propValues.map((entry, index) => (
+      <Col key={index} xs={12} md={8} mdOffset={(index != 0 ? 4 : undefined)} >
+        {entry.type.endsWith('CustomPropertyRefdata') ?  entry.value.value : entry.value }
+      </Col>
+    ));
+  }
 
-const renderCustomProperties = (customProperties) => {
-  const propertyList = [];
-  Object.keys(customProperties).forEach((propertyName) => {
-    const entries = customProperties[propertyName];
-    propertyList.push(
-      <Row className={css.licenseCustomPropsRow}>
-        <Col xs={12} md={4} className={css.licenseCustomPropName}>{nicePropertyName(propertyName)}</Col>
-        { renderCustomPropertyValues(entries) }
-      </Row>
+  renderProperties = () => {
+    return Object.entries(this.props.license.customProperties)
+      .map(([propName, propValues], index) => (
+        <Layout className="padding-top-gutter" key={index}>
+          <Row>
+            {this.renderPropertyName(propName)}
+            {this.renderPropertyValues(propValues)}
+          </Row>
+        </Layout>
+      ));
+  }
+
+  render() {
+    const { id, license, onToggle, open } = this.props;
+    if (!license.customProperties || Object.keys(license.customProperties).length === 0) return null;
+
+    return (
+      <Accordion
+        id={id}
+        label={<FormattedMessage id="ui-licenses.licenses.customProperties" />}
+        open={open}
+        onToggle={onToggle}
+      >
+        {this.renderProperties(license.customProperties)}
+      </Accordion>
     );
-  });
 
-  return propertyList;
-};
-
-const LicenseCustomProperties = ({ license, id, onToggle, open }) => (
-  <Accordion id={id} label={<FormattedMessage id="ui-licenses.licenses.customProperties" />} open={open} onToggle={onToggle}>
-    {renderCustomProperties(license.customProperties)}
-  </Accordion>
-);
-
-LicenseCustomProperties.propTypes = {
-  license: PropTypes.shape({ customProperties: PropTypes.object }),
-  id: PropTypes.string,
-  onToggle: PropTypes.func,
-  open: PropTypes.bool,
-};
-
-export default LicenseCustomProperties;
+  }
+}
