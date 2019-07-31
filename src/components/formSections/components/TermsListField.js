a8d3aa84285f3676a812d42af9e62a7465ac2041
Claudia Malzer <malzer@ulb.tu-darmstadt.de>
2019-07-31 19:05:54 +0200

    ERM-355: Manage public notes on license term values
    fix label and required issue

--- src/components/formSections/components/TermsListField.js
+++ src/components/formSections/components/TermsListField.js
@@ -6,7 +6,6 @@ import {
   Card,
   Col,
   IconButton,
-  Label,
   Row,
   Select,
   TextArea,
@@ -85,43 +84,39 @@ class TermsListField extends React.Component {
     });
 
     return (
-      <div>
-        <Label htmlFor={`edit-term-${i}-name`}>
-          <FormattedMessage id="ui-licenses.prop.termName" />
-        </Label>
-        <Select
-          data-test-term-name
-          dataOptions={[term, ...unsetTerms]} // The selected term, and the available unset terms
-          id={`edit-term-${i}-name`}
-          onChange={e => {
-            const newValue = e.target.value;
+      <Select
+        data-test-term-name
+        dataOptions={[term, ...unsetTerms]} // The selected term, and the available unset terms
+        id={`edit-term-${i}-name`}
+        label={<FormattedMessage id="ui-licenses.prop.termName" />}
+        onChange={e => {
+          const newValue = e.target.value;
 
-            // Update `state.terms` which controls what terms are being edited.
-            this.setState(prevState => {
-              const newTerms = [...prevState.terms];
-              newTerms[i] = this.getTerm(newValue);
+          // Update `state.terms` which controls what terms are being edited.
+          this.setState(prevState => {
+            const newTerms = [...prevState.terms];
+            newTerms[i] = this.getTerm(newValue);
 
-              return { terms: newTerms };
-            });
+            return { terms: newTerms };
+          });
 
-            // Update redux-form (which tracks what the values for a given term are) because
-            // in essence we're deleting a term and creating a new term.
-            // We do this by 1) marking the current term for deletion and 2) initing
-            // the new term to an empty object.
-            const currentValue = value[term.value] ? value[term.value][0] : {};
-            onChange({
-              ...value,
-              [term.value]: [{
-                id: currentValue.id,
-                _delete: true,
-              }],
-              [newValue]: [{}],
-            });
-          }}
-          required
-          value={term.value}
-        />
-      </div>
+          // Update redux-form (which tracks what the values for a given term are) because
+          // in essence we're deleting a term and creating a new term.
+          // We do this by 1) marking the current term for deletion and 2) initing
+          // the new term to an empty object.
+          const currentValue = value[term.value] ? value[term.value][0] : {};
+          onChange({
+            ...value,
+            [term.value]: [{
+              id: currentValue.id,
+              _delete: true,
+            }],
+            [newValue]: [{}],
+          });
+        }}
+        required
+        value={term.value}
+      />
     );
   }
 
@@ -167,47 +162,29 @@ class TermsListField extends React.Component {
     };
 
     return (
-      <div>
-        <Label htmlFor="edit-term-value">
-          <FormattedMessage id="ui-licenses.prop.termValue" />
-        </Label>
-        <FieldComponent
-          data-test-term-value
-          id={`edit-term-${i}-value`}
-          onChange={handleChange}
-          value={controlledFieldValue}
-          error={errorMessage}
-          {...fieldProps}
-        />
-      </div>
+      <FieldComponent
+        data-test-term-value
+        id={`edit-term-${i}-value`}
+        label={<FormattedMessage id="ui-licenses.prop.termValue" />}
+        onChange={handleChange}
+        value={controlledFieldValue}
+        error={errorMessage}
+        {...fieldProps}
+      />
     );
   }
 
   renderTermVisibility = (term, i) => {
-    /* const { input: { onChange, value } } = this.props;
-    const currentValue = term.internal ? term.internal : true; */
     const { input: { onChange, value } } = this.props;
     const termObject = value[term.value] ? value[term.value][0] : {};
     const { internal } = termObject;
 
-    // Initialise to just the value (for text/number values)
-    // and then check if it's an object (for Select/refdata values).
-    /* let controlledFieldValue = currentValue.value;
-    if (controlledFieldValue && controlledFieldValue.value) {
-      controlledFieldValue = controlledFieldValue.value;
-    } */
-
-    const FieldComponent = Select;
-    /*  const fieldProps = {};
-    fieldProps.dataOptions = term.options; */
-
     const dataOptions = [
-      { value: true, label: this.props.intl.formatMessage({ id: "ui-licenses.term.internalTrue" }) },
-      { value: false, label: this.props.intl.formatMessage({ id: "ui-licenses.term.internalFalse" }) }
+      { value: true, label: this.props.intl.formatMessage({ id: 'ui-licenses.term.internalTrue' }) },
+      { value: false, label: this.props.intl.formatMessage({ id: 'ui-licenses.term.internalFalse' }) }
     ];
 
     const handleChange = e => {
-      console.log(typeof(e.target.value),'value');
       onChange({
         ...value,
         [term.value]: [{
@@ -217,30 +194,16 @@ class TermsListField extends React.Component {
       });
     };
 
-    /* const handleChange = e => {
-      onChange({
-        ...value,
-        [term.internal]: [{
-          ...currentValue,
-          value: e.target.internal
-        }],
-      });
-    }; */
-
     return (
-      <div>
-        <Label htmlFor="edit-term-visibility">
-          <FormattedMessage id="ui-licenses.prop.termVisibility" />
-        </Label>
-        <FieldComponent
-          data-test-term-visibility
-          id={`data-test-term-${i}-visibility`}
-          dataOptions={dataOptions}
-          onChange={handleChange}
-          value={internal}
-          required
-        />
-      </div>
+      <Select
+        data-test-term-visibility
+        id={`data-test-term-${i}-visibility`}
+        dataOptions={dataOptions}
+        label={<FormattedMessage id="ui-licenses.prop.termVisibility" />}
+        onChange={handleChange}
+        value={internal}
+        required
+      />
     );
   }
 
@@ -291,18 +254,14 @@ class TermsListField extends React.Component {
     };
 
     return (
-      <div>
-        <Label htmlFor={`edit-term-${i}-internal-note`}>
-          <FormattedMessage id="ui-licenses.term.internalNote" />
-        </Label>
-        <TextArea
-          data-test-term-internal-note
-          fullWidth
-          id={`edit-term-${i}-internal-note`}
-          onChange={handleChange}
-          value={note}
-        />
-      </div>
+      <TextArea
+        data-test-term-internal-note
+        fullWidth
+        id={`edit-term-${i}-internal-note`}
+        label={<FormattedMessage id="ui-licenses.term.internalNote" />}
+        onChange={handleChange}
+        value={note}
+      />
     );
   }
 
@@ -322,18 +281,14 @@ class TermsListField extends React.Component {
     };
 
     return (
-      <div>
-        <Label htmlFor={`edit-term-${i}-public-note`}>
-          <FormattedMessage id="ui-licenses.term.publicNote" />
-        </Label>
-        <TextArea
-          data-test-term-public-note
-          fullWidth
-          id={`edit-term-${i}-public-note`}
-          onChange={handleChange}
-          value={publicNote}
-        />
-      </div>
+      <TextArea
+        data-test-term-public-note
+        fullWidth
+        id={`edit-term-${i}-public-note`}
+        onChange={handleChange}
+        label={<FormattedMessage id="ui-licenses.term.publicNote" />}
+        value={publicNote}
+      />
     );
   }
 
