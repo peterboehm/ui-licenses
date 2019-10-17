30b200585f1c35aae5c9f3c6cfb4f1d896d5de4a
Aditya Matukumalli <aditya.matukumalli@gmail.com>
2019-10-17 13:21:40 -0400

    Remove repeated fields from the license header, refs ERM-517

--- src/components/viewSections/LicenseInfo.js
+++ src/components/viewSections/LicenseInfo.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { get } from 'lodash';
 import { FormattedMessage } from 'react-intl';
 import { Col, Headline, KeyValue, Row } from '@folio/stripes/components';
-import { LicenseEndDate } from '@folio/stripes-erm-components';
-
-import FormattedUTCDate from '../FormattedUTCDate';
 
 class LicenseInfo extends React.Component {
   static propTypes = {
@@ -28,36 +24,6 @@ class LicenseInfo extends React.Component {
                 {license.name}
               </Headline>
             </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={6} md={3}>
-            <KeyValue label={<FormattedMessage id="ui-licenses.prop.type" />}>
-              <div data-test-license-type>
-                {get(license, 'type.label', '-')}
-              </div>
-            </KeyValue>
-          </Col>
-          <Col xs={6} md={3}>
-            <KeyValue label={<FormattedMessage id="ui-licenses.prop.status" />}>
-              <div data-test-license-status>
-                {get(license, 'status.label', '-')}
-              </div>
-            </KeyValue>
-          </Col>
-          <Col xs={6} md={3}>
-            <KeyValue label={<FormattedMessage id="ui-licenses.prop.startDate" />}>
-              <div data-test-license-start-date>
-                {license.startDate ? <FormattedUTCDate value={license.startDate} /> : '-'}
-              </div>
-            </KeyValue>
-          </Col>
-          <Col xs={6} md={3}>
-            <KeyValue label={<FormattedMessage id="ui-licenses.prop.endDate" />}>
-              <div data-test-license-end-date>
-                <LicenseEndDate license={license} />
-              </div>
-            </KeyValue>
           </Col>
         </Row>
         <Row>
