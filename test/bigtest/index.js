41f2d2c3a9d0d7952bf81564dade848e1c54a9b8
Mark Deutsch <mark.deutsch@duke.edu>
2020-03-04 13:26:57 -0500

    Merge in BigTest infrastructure

---
+++ test/bigtest/index.js
@@ -0,0 +1,10 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import turnOffWarnings from './helpers/turn-off-warnings';
+
+turnOffWarnings();
+
+// require all modules ending in "-test" from the current directory and
+// all subdirectories
+const requireTest = require.context('./tests/', true, /-test/);
+requireTest.keys().forEach(requireTest);
